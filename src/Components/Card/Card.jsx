5b4a30ab7871ab74a16d34a3515bf04fd7474da1
Owen Na <newseoul1302@gmail.com>
2023-04-10 19:18:18 -0400

    added one main card, check comments in card.css

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import '../../styles/card.css'
+
+export default function Card() {
+    return (
+        <div className="parent">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
