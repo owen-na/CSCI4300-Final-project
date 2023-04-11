b9e30b2da81eb9e8c6fcc53e977ff58193dea9d1
owen <osn53387@uga.edu>
2023-04-11 15:59:31 -0400

    basic html for signUp page

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,8 +1,26 @@
-import "../styles/login.css"
+import "../styles/login.css";
 
 export default function SignIn() {
-    return (
-      <div className="signin">
+  return (
+    <>
+      <div>
+        <button>
+          <p> &lt; back</p>
+        </button>
       </div>
-    );
-  }
+      <div className="signUnForm">
+        <form>
+          <h1>Login</h1>
+          <label for="email">email</label>
+          <input type="email" name="email" required></input>
+          <label for="password">password</label>
+          <input type="password" name="password" requried></input>
+          <button type="submit">sign up</button>
+        </form>
+        <p>
+          already a user? <a href="./SignIn.jsx">login</a>
+        </p>
+      </div>
+    </>
+  );
+}
