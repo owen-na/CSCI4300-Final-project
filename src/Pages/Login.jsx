1912bed3f7fe4ae84cf28794e6721a790c1eaa58
owen <osn53387@uga.edu>
2023-04-11 15:26:41 -0400

    finished setup for login.jsx, just need styling

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,27 @@
+export default function Login() {
+  return (
+    <>
+      <div>
+        <button>
+          <p> &lt; back</p>
+        </button>
+      </div>
+      <div className="loginForm">
+        <form>
+          <h1>Login</h1>
+          <label for="email">email</label>
+          <input type="email" name="email" required></input>
+          <label for="password">password</label>
+          <input type="password" name="password" requried></input>
+          <button type="submit">login</button>
+        </form>
+        <p>
+          need an account? <a href="./SignIn.jsx">sign up</a>
+        </p>
+      </div>
+    </>
+  );
+}
+
+// need onSession checker
+// need to make sure form connects to db later
