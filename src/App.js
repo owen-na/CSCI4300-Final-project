8b43e377a9ac00ffadd63c4eca89c32bcad5bd0d
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-04-11 17:28:30 -0400

    change img

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Card from './Components/Card/Card'
-import Header from './Components/Header/Header';
-import './App.css';
-
+import Card from "./Components/Card/Card";
+import Header from "./Components/Header/Header";
+import "./App.css";
+import arrow from "./assets/arrow.svg";
 function App() {
   return (
     <>
-    <Header />
-    <div className="mainParent">
-      <button type='button'>
-        <img src="./public/assets/724927.png" alt="Backarrow" className="backArrow"/>
-      </button>
-      <div className="horizontalScroll">
-        <Card />
+      <Header />
+      <div className="mainParent">
+        <button type="button">
+          <img src={arrow} alt="Backarrow" className="barrow" />
+        </button>
+        <div className="horizontalScroll">
+          <Card />
+        </div>
+        <button type="button">
+          <img src={arrow} alt="arrow" className="farrow" />
+        </button>
       </div>
-      <button type='button'>
-        <img src="./public/assets/724927.png" alt="arrow" />
-      </button>
-    </div>
     </>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
       <div className="links">
-        <a href="../Pages/SignIn.jsx">Sign Up</a>
-        <a href="../Pages/Login.jsx">Login</a>
+        <a href="../Pages/SignIn">Sign Up</a>
+        <a href="../Pages/Login">Login</a>
       </div>
     </div>
   );
