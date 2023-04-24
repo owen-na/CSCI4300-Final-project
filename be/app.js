5cf689b64c99a2d9572a116777df37e675127f60
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-04-23 20:51:31 -0400

    model update

--- be/app.js
+++ be/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-require("dotenv").config({ path: "../.env", debug: true });
+const Card = require("./models/cardModel");
 const app = express();
 
 const uri = `mongodb+srv://steventran:${process.env.REACT_APP_PASSWORD}@cluster0.oeg5zlf.mongodb.net/?retryWrites=true&w=majority`;
@@ -16,9 +14,13 @@ async function connect() {
     console.log(error);
   }
 }
-app.post("/cards", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post("/card", async (req, res) => {
+  try {
+    const card = await Card.create(req.body);
+    res.status(200).json(card);
+  } catch (error) {
+    console.log(error);
+  }
 });
 connect();
 app.listen(8000, () => {
